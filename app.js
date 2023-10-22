cb402bde7cd613a25e642d5dffef6df287d1ca62
ivanysea <ivanysea@hotmail.com>
2023-10-22 17:31:44 -0300

    segunda pre-entrega

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ const direccion = document.querySelector(".direccion")
 
 const nuevaCard = document.createElement(`div`)
 
+nuevaCard.className += "card direccion"
+
+nuevaCard.style.width = "18rem"
+
 nuevaCard.innerHTML= (`<div class="card-body">
     <h5 class="card-title">MemoriaRam 8ghz</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
